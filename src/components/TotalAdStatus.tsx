bbe9b398852dbb4669f5fdc383a9e96a03f4ceeb
hyebinjo <hyebinjo91@gmail.com>
2022-07-14 17:16:25 +0900

    feat: 주간데이터 계산하는 함수구현, 데이터 뿌려주기

--- src/components/TotalAdStatus.tsx
+++ src/components/TotalAdStatus.tsx
@@ -3,58 +3,64 @@ import { theme } from '../theme';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { Total } from './TotalDataExample';
 
-const newTotal = Total.map((data) => {
-  const date = new Date(data.date);
-  const dateString = `${date.getMonth() + 1}월 ${date.getDate()}일`;
-  return { ...data, dateformat: dateString };
-});
+type DailyData = {
+  imp: number;
+  click: number;
+  cost: number;
+  conv: number;
+  convValue: number;
+  ctr: number;
+  cvr: number;
+  cpc: number;
+  cpa: number;
+  roas: number;
+  date: string;
+};
+
+type WeekArr = DailyData[];
+
+type singleDataCard = { name: string; value: string; change: string };
+
+type WeeklyTotalData = singleDataCard[];
 
 export default function TotalAdStatus() {
+  const newTotal = Total.map((data) => {
+    const date = new Date(data.date);
+    const dateString = `${date.getMonth() + 1}월 ${date.getDate()}일`;
+    return { ...data, dateformat: dateString };
+  });
+
+  const getWeeklyTotalData = (weekArr: WeekArr): WeeklyTotalData => {
+    const roas = parseInt(`${weekArr.reduce((a, b) => a + b.roas, 0) / 7}`);
+    const cost = weekArr.reduce((a, b) => a + b.cost, 0).toLocaleString();
+    const imp = weekArr.reduce((a, b) => a + b.imp, 0).toLocaleString();
+    const click = weekArr.reduce((a, b) => a + b.click, 0).toLocaleString();
+    const conv = weekArr.reduce((a, b) => a + b.conv, 0).toLocaleString();
+    const convValue = weekArr.reduce((a, b) => a + b.convValue, 0).toLocaleString();
+    const weeklyData = [
+      { name: 'ROAS', value: `${roas}%`, change: '▲18%' },
+      { name: '광고비', value: `${cost}원`, change: '▲18%' },
+      { name: '노출수', value: `${imp}회`, change: '▲18%' },
+      { name: '클릭수', value: `${click}회`, change: '▲18%' },
+      { name: '전환수', value: `${conv}회`, change: '▲18%' },
+      { name: '매출', value: `${convValue}원`, change: '▲18%' },
+    ];
+    return weeklyData;
+  };
+  const weeklyTotalData = getWeeklyTotalData(Total);
+
   return (
     <Container>
       <CardContainer>
-        <Card>
-          <dt>ROAS</dt>
-          <div>
-            <dd className="value">600%</dd>
-            <dd className="change">▲18%</dd>
-          </div>
-        </Card>
-        <Card>
-          <dt>ROAS</dt>
-          <div>
-            <dd className="value">600%</dd>
-            <dd className="change">▲18%</dd>
-          </div>
-        </Card>
-        <Card>
-          <dt>ROAS</dt>
-          <div>
-            <dd className="value">600%</dd>
-            <dd className="change">▲18%</dd>
-          </div>
-        </Card>
-        <Card>
-          <dt>ROAS</dt>
-          <div>
-            <dd className="value">600%</dd>
-            <dd className="change">▲18%</dd>
-          </div>
-        </Card>
-        <Card>
-          <dt>ROAS</dt>
-          <div>
-            <dd className="value">600%</dd>
-            <dd className="change">▲18%</dd>
-          </div>
-        </Card>
-        <Card>
-          <dt>ROAS</dt>
-          <div>
-            <dd className="value">600%</dd>
-            <dd className="change">▲18%</dd>
-          </div>
-        </Card>
+        {weeklyTotalData.map((data: singleDataCard) => (
+          <Card>
+            <dt>{data.name}</dt>
+            <div>
+              <dd className="value">{data.value}</dd>
+              <dd className="change">{data.change}</dd>
+            </div>
+          </Card>
+        ))}
       </CardContainer>
       <div>
         <button>roas</button>
@@ -77,7 +83,7 @@ export default function TotalAdStatus() {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="roas" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="roas" stroke="#8884d8" />
           <Line type="monotone" dataKey="click" stroke="#82ca9d" />
         </LineChart>
       </ResponsiveContainer>
@@ -99,7 +105,7 @@ const CardContainer = styled.dl`
   flex-wrap: wrap;
 `;
 const Card = styled.div`
-  width: 28%;
+  width: 12rem;
   padding: 0.5rem 1rem;
   margin-bottom: 1rem;
   border: 1px solid rgb(235, 235, 235);
