6a83d64f4f57adac65ed590e22a501eeffdc207e
Avoler0 <91608021+Avoler0@users.noreply.github.com>
2022-07-15 11:09:06 +0900

    Fix : 매체현황 , 차트데이터 재정렬 (#6)
    * Feat: 매체 현황 data 병합 관리 , 차트 준비
    
    * Fix: 차트데이터 부분 재정렬
    
    Co-authored-by: seongjin <77876601+ysjkof@users.noreply.github.com>

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from '../Layout';
+import MediaStatus from '../components/MediaStatus';
 import Test from '../pages/Test';
 
 function Router() {
@@ -8,7 +9,7 @@ function Router() {
       <Routes>
         <Route path="" element={<Layout />}>
           <Route index element={<p>대시보드</p>} />
-          <Route path="dashboard" element={<p>대시보드</p>} />
+          <Route path="dashboard" element={<MediaStatus />} />
           <Route path="management" element={<p>광고관리</p>} />
           <Route path="*" element={<div>페이지가 없습니다</div>} />
         </Route>
