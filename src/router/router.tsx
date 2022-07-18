17f0ec1dcc3502ff7d3b8ccbf084af0f18749616
YOUNGHO8762 <YOUNGHO8762@gmail.com>
2022-07-18 17:27:06 +0900

    feat : 광고만들기 페이지 로직 완성

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import MediaStatus from '../components/MediaStatus';
 import Layout from '../Layout';
@@ -6,6 +5,7 @@ import Main from '../pages/Main';
 import Management from '../pages/Management';
 import Test from '../pages/Test';
 import Dashboard from '../pages/Dashboard';
+import CreateAd from '../pages/CreateAd';
 
 function Router() {
   return (
@@ -14,7 +14,9 @@ function Router() {
         <Route path="" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="management" element={<Management />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          {/* <Route path="dashboard" element={<Dashboard />} /> */}
+          {/* <Route path="create" element={<CreateAd />} /> */}
+          <Route path="dashboard" element={<CreateAd />} />
           <Route path="*" element={<div>페이지가 없습니다</div>} />
         </Route>
         <Route path="/test" element={<Test />} />
