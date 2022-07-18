fcad18ff8367893ff9361fc0be043a0d7500acc8
wgnator <wgnator@gmail.com>
2022-07-18 14:56:48 +0900

    temp

--- src/utils/getProcessedData.ts
+++ src/utils/getProcessedData.ts
@@ -1,8 +1,13 @@
 import integrationStatusServices from '../api/integrationStatusServices';
+import { format } from 'date-fns';
 
 type StartAndEndDate = { startDate: Date; endDate: Date };
 
 export const getDataRangeByDate = async (): Promise<StartAndEndDate> => {
   const data = await integrationStatusServices.getAll().then((data) => data.report.daily);
   return { startDate: new Date(data[0].date), endDate: new Date(data[data.length - 1].date) };
+};
+
+export const convertTwoDatesToString = (startDate: Date, endDate: Date) => {
+  return format(startDate, 'yyyyMMdd') + '~' + format(endDate, 'yyyyMMdd');
 };
