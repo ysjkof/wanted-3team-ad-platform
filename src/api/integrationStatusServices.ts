ec9303a37d70a1fde1af2b4820acc3ce353d6371
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-07-18 15:15:59 +0900

    refactor: API, model, httpRequest 역할분담 개선 (#14)
    * rename: 파일, 함수 이름 변경
    
    - intergrationStatus -> totalAdStatus로 변경
    
    * refactor: API, model, httpRequest 역할분담 개선

--- src/api/integrationStatusServices.ts
+++
@@ -1,6 +0,0 @@
-import { END_POINT_INTEGRATED_ADVERTISING_REPORTS } from '../constants/constants';
-import { IntegrationStatus } from '../databaseTypes';
-import { axiosInstance } from './common/axiosInstance';
-import { HttpRequest } from './common/httpRequest';
-
-export default new HttpRequest<IntegrationStatus>(axiosInstance, END_POINT_INTEGRATED_ADVERTISING_REPORTS);
