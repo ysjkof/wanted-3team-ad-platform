ec35b04bb3a13593fe8f7e29f871ba54911ac6c3
wgnator <69628701+wgnator@users.noreply.github.com>
2022-07-19 15:28:41 +0900

    feat: 통합광고현황 전 기간(주/월) 데이타 같이 받아오게 useTotalAdStatus 훅 변경 (#21)
    * temp
    
    * temp
    
    * feat: 통합광고현황 및 매체현황 데이터 캐싱기능 구현
    
    * temp
    
    * feat: 통합광고현황 전 기간(주/월) 데이타 같이 받아오기
    
    Co-authored-by: wgnator <wgnator@gmail.com>

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,22 +7,31 @@ export default function Main() {
   return (
     <Container>
       <Header />
-      <Content className="content">
-        <Outlet />
-      </Content>
+      <Wrapper>
+        <Content className="content">
+          <Outlet />
+        </Content>
+      </Wrapper>
     </Container>
   );
 }
 
 const Container = styled.div`
+  height: 100vh;
   width: 80%;
   background-color: ${theme.mainBackgroundColor};
   @media (max-width: 480px) {
     width: 100%;
   }
 `;
-
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  overflow: auto;
+`;
 const Content = styled.div`
+  position: relative;
+  top: 3rem;
   @media (max-width: 480px) {
     position: relative;
     top: 15vw;
