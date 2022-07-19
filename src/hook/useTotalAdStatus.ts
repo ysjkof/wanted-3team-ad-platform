ef7f1426cc2e81c5e78baaf63e542e47d6d207b8
wgnator <wgnator@gmail.com>
2022-07-19 15:22:02 +0900

    feat: 통합광고현황 전 기간(주/월) 데이타 같이 받아오기

--- src/hook/useTotalAdStatus.ts
+++ src/hook/useTotalAdStatus.ts
@@ -3,23 +3,51 @@ import { QueryOptions } from '../api/httpRequest';
 import { DailyAdStatus } from '../interfaces/database';
 import totalAdStatusModel from '../models/totalAdStatusModel';
 import { useCache } from './useCache';
+import { differenceInCalendarDays, getMonth, lastDayOfMonth, startOfMonth, subDays, subMonths } from 'date-fns';
 
-function useTotalAdStatus(queryOptions: QueryOptions) {
-  const [totalAdStatus, setTotalAdStatus] = useState<DailyAdStatus[]>();
+type DailyAdStatusWithPrev = {
+  prev: DailyAdStatus[] | null;
+  curr: DailyAdStatus[];
+};
+
+function useTotalAdStatus(queryOptions?: QueryOptions) {
+  const [totalAdStatus, setTotalAdStatus] = useState<DailyAdStatusWithPrev>();
   const [loading, setLoading] = useState(true);
   const { returnDataIfExistInCache, saveInCacheAndReturnData } = useCache<DailyAdStatus[]>();
 
   const getTotalAdStatus = async (queryOptions: QueryOptions) => {
     setLoading(true);
-    setTotalAdStatus(
-      returnDataIfExistInCache(queryOptions) ||
-        saveInCacheAndReturnData(queryOptions, await totalAdStatusModel.getPeriod(queryOptions)),
-    );
+
+    const differenceInDays = differenceInCalendarDays(queryOptions.lte, queryOptions.gte) + 1;
+
+    const prevWeekQuery =
+      differenceInDays <= 7
+        ? {
+            gte: subDays(queryOptions.gte, differenceInDays),
+            lte: subDays(queryOptions.lte, differenceInDays),
+          }
+        : {
+            gte: startOfMonth(subMonths(queryOptions.gte, 1)),
+            lte: lastDayOfMonth(subMonths(queryOptions.gte, 1)),
+          };
+
+    const currWeekQuery = queryOptions;
+
+    const prevTermData =
+      returnDataIfExistInCache(prevWeekQuery) ||
+      saveInCacheAndReturnData(prevWeekQuery, await totalAdStatusModel.getPeriod(prevWeekQuery));
+
+    const currTermData =
+      returnDataIfExistInCache(currWeekQuery) ||
+      saveInCacheAndReturnData(currWeekQuery, await totalAdStatusModel.getPeriod(currWeekQuery));
+
+    setTotalAdStatus({ prev: prevTermData.length ? prevTermData : null, curr: currTermData });
+
     setLoading(false);
   };
 
   useEffect(() => {
-    getTotalAdStatus(queryOptions);
+    queryOptions ? getTotalAdStatus(queryOptions) : null;
   }, []);
 
   return { loading, totalAdStatus, getTotalAdStatus };
